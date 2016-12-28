de03a3bb983ba3b773502a278039490dc0ab31a2
award28 <ward.austin28@gmail.com>
2016-12-28 00:19:07 -0500

    Homepage input finished

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,23 +1,12 @@
 var app = angular.module("myApp", []);
 
-app.controller('notes', ['$scope', '$http', '$window', function($scope, $http, $window) {  
-
-    $scope.getIng = function() {
-        $http.get('/notebook').success(function(response) {
-            $window.alert(response);
-        }).error(function(error, status) {
-            $window.alert(error);
-        });
-    }
-
-    $scope.clickMe = function() {
-        $http.post('/notebook', $scope.note).success(function(response) {
-            $scope.note = "";
-            $scope.ingredients = response;
-        }).error(function(error, status) {
-            $window.alert(error);
-        });
-    }
-    
+app.controller('index', ['$scope', '$http', '$window', '$document', function($scope, $http, $window, $document) {
+    $scope.$watch('search', function(search) {
+        if(search != undefined && search != "") 
+            $scope.searchBg={"background": "#fe5f55"}
+        
+        else 
+            $scope.searchBg={}
+    });
 }]);
 
