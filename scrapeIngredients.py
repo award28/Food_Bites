61f156e03c18de23c578bc118ad62ee440481daf
jfreynolds <jreynolds99639@gmail.com>
2016-12-29 17:43:19 -0500

    Added price/serving scrapper

--- scrapeIngredients.py
+++ scrapeIngredients.py
@@ -16,3 +16,16 @@ if __name__ == '__main__':
 
     for ing in ingredients:
         print(ing)
+
+    totalPrice = soup.find_all('span', attrs={'itemprop': 'recipeCategory'})
+    pricePerServing = soup.find_all('span', attrs={'itemprop': 'recipeCuisine'})
+    totalServings = soup.find_all('span', attrs={'itemprop': 'recipeYield'})
+
+    for span in totalPrice:
+        print('Total Cost = ' + span.string)
+
+    for span in pricePerServing:
+        print('Price Per Serving = ' + span.string)
+
+    for span in totalServings:
+        print('Total Servings = ' + span.string)
